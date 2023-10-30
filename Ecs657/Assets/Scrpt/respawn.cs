69cc527e5faa0a7a558cc3dccce0347ce69ff9c3
Remy-Macdermott <71390314+Remy-Macdermott@users.noreply.github.com>
2023-10-30 16:03:56 +0000

    Respawn
    added respawn point and respawn when falling off of map

---
+++ Ecs657/Assets/Scrpt/respawn.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Entered");
+        if (other.CompareTag("Player"))
+            player.transform.position = respawnPoint.transform.position;
+        }
+    }
+}
