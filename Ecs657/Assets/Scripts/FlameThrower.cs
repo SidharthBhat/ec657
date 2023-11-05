39142faea23725c874f23cd8a40867aab67b1791
Remy-Macdermott <71390314+Remy-Macdermott@users.noreply.github.com>
2023-11-05 17:54:56 +0000

    flamethrower adjusments
    fixed and improved visuals for flamethrower

--- Ecs657/Assets/Scripts/FlameThrower.cs
+++ Ecs657/Assets/Scripts/FlameThrower.cs
@@ -2,45 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExampleClass : MonoBehaviour
+public class FlamethrowerController : MonoBehaviour
 {
-    // Declare and set particle System
-    ParticleSystem system;
-    bool firstHit = false;
+    public ParticleSystem flameParticles;
+    public int damage = 10;
 
-    private void Start()
+    void Update()
     {
-        system = this.GetComponent<ParticleSystem>();
+        if (Input.GetButtonDown("Fire1")) // Change to your desired input method
+        {
+            StartFlamethrower();
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            StopFlamethrower();
+        }
     }
 
-    // See Order of Execution for Event Functions for information on FixedUpdate() and Update() related to physics queries
-    void FixedUpdate()
+    void StartFlamethrower()
     {
-        // Bit shift the index of the layer (8) to get a bit mask
-        int layerMask = 1 << 8;
+        flameParticles.Play();
+    }
 
-        // This would cast rays only against colliders in layer 8.
-        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-        layerMask = ~layerMask;
+    void StopFlamethrower()
+    {
+        flameParticles.Stop();
+    }
+    // Detect collisions with the Particle System
+    void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("Particle collided with: " + other.name);
+        if (other.gameObject.GetComponent<Enemy>() != null)
+        {
+            other.gameObject.GetComponent<Enemy>().TakeDamage(damage);
+        }
 
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-        {
-            if (!firstHit)
-            {
-                firstHit = true;
-                system.Stop();
-                system.Play();
-            }
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Debug.Log("Did Hit");
-        }
-        else
-        {
-            firstHit = false;
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            Debug.Log("Did not Hit");
-        }
     }
 }
